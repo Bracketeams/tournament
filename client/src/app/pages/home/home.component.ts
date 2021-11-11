ba27d15fa576c6c2ff2ef48b9365dda5b69534c6
Katherine Min <msyeon1028@gmail.com>
2021-11-11 18:33:00 -0500

    Create tournament class and view (Read).

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+<<<<<<< Updated upstream
+=======
+import { ActivatedRoute } from '@angular/router';
+import { BasePageComponent } from 'src/app/partials/base-page/base-page.component';
+import { TournamentService } from 'src/app/services/tournament.service';
+>>>>>>> Stashed changes
 
 @Component({
   selector: 'app-home',
@@ -9,7 +15,10 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+  constructor(private tournamentService:TournamentService) { }
+
   ngOnInit(): void {
+    this.tounaments = this.tournamentService.onGet();
   }
 
 }
