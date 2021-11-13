e20ea4af16a769421cfefe46d95bbd05919fd154
Gabriel Tinoco <gabrielslai@gmail.com>
2021-11-13 19:34:07 -0300

    Fixed routing for the temporary pages.

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { TournamentsComponent } from './pages/tournaments/tournaments.component';
 
 // Routing goes here.
 const routes: Routes = [
   {path: 'home', component: HomeComponent, data: {title: 'Home'}},
-  {path: '', redirectTo: "/home", pathMatch: 'full'},
-  {path: 'tournament/add/:id', component: EditComponent},
-  {path: 'tournament/edit/:id', component: EditComponent}
+  {path: 'tournaments', component: TournamentsComponent, data: {title: 'Tournaments'}},
+  {path: '', redirectTo: "/home", pathMatch: 'full'}
+  //{path: 'tournament/add/:id', component: EditComponent},
+  //{path: 'tournament/edit/:id', component: EditComponent}
 ];
 
 @NgModule({
