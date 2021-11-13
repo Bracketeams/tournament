ac6268aba6cf2a094603edee86fd201435fb6ced
Gabriel Tinoco <gabrielslai@gmail.com>
2021-11-13 19:30:33 -0300

    Implemented backend mockdata, mapping, and code for future authentication services.

---
+++ client/mockdata.js
@@ -0,0 +1,21 @@
+module.exports = function() {
+    return {
+        tournaments: [
+            {
+                "id": 1,
+                "name": "Cooltournament",
+                "entry": ["mark", "sales", "rony", "bruna"]
+            },
+            {
+                "id": 2,
+                "name": "Cooltournament2",
+                "entry": ["mark", "sales", "rony", "bruna"]
+            },
+            {
+                "id": 3,
+                "name": "Cooltournament3",
+                "entry": ["mark", "sales", "rony", "bruna"]
+            }
+        ]
+    }
+}
